f32aac68711ae6207e46a1318b5d2974ae227483
Joe Fabisevich <Joe@fabisevi.ch>
2014-01-17 22:12:12 -0500

    Initial commit

---
+++ JFFileManager.h
@@ -0,0 +1,58 @@
+//
+//  JFFileManager.h
+//  Reminders
+//
+//  Created by Joe on 5/24/13.
+//  Copyright (c) 2013 mergesort All rights reserved.
+//
+
+
+#import <Foundation/Foundation.h>
+
+@interface JFFileManager : NSObject
+
+/**
+ A file location for an application's main bundle.
+ */
++ (NSString *)mainBundleDirectory;
+
+/**
+ A file location for an application's caches directory.
+ */
++ (NSString *)cachesDirectory;
+
+/**
+ A file location for an application's documents directory.
+ */
++ (NSString *)documentsDirectory;
+
+/**
+ A file location for an application's temporary directory.
+ */
++ (NSString *)temporaryDirectory;
+
+/**
+ A file location for serializing an application's user object to disk.
+ */
++ (NSString *)userFile;
+
+/**
+ A file location for an application's main bundle
+ 
+ @param fileData The data to write to a file.
+ @param path The path to write the data to.
+ @return BOOL Whether or not th file was created.
+ */
++ (BOOL)createFile:(NSData *)fileData atPath:(NSString *)path;
+
+/**
+ A file location for an application's main bundle
+ 
+ @param path The path to remove the file from.
+ @return BOOL Whether or not the file was deleted.
+ */
++ (BOOL)removeFileAtPath:(NSString *)path;
+
++ (NSString *)contentsOfFileAtPath:(NSString *)path;
+
+@end
---
+++ JFFileManager.m
@@ -0,0 +1,62 @@
+//
+//  JFFileManager.m
+//  Reminders
+//
+//  Created by Joe on 5/24/13.
+//  Copyright (c) 2013 mergesort All rights reserved.
+//
+
+
+#import "JFFileManager.h"
+
+@implementation JFFileManager
+
++ (NSString *)mainBundleDirectory
+{
+    return [NSBundle mainBundle].resourcePath;
+}
+
++ (NSString *)cachesDirectory
+{
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
+	NSString *cachePath = [paths firstObject];
+	return cachePath;    
+}
+
++ (NSString *)documentsDirectory
+{
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectoryPath = [paths firstObject];
+    return documentsDirectoryPath;    
+}
+
++ (NSString *)temporaryDirectory
+{
+    return NSTemporaryDirectory();
+}
+
++ (NSString *)userFile
+{
+    return [[self documentsDirectory] stringByAppendingPathComponent:@"User"];
+}
+
++ (BOOL)createFile:(NSData *)fileData atPath:(NSString *)path
+{
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    return [fileManager createFileAtPath:path contents:file attributes:nil];
+}
+
++ (BOOL)removeFileAtPath:(NSString *)path
+{
+    NSError *error;
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    return [fileManager removeItemAtPath:path error:&error];
+}
+
++ (NSString *)contentsOfFileAtPath:(NSString *)path
+{
+    NSString* content = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
+    return content;
+}
+
+@end
